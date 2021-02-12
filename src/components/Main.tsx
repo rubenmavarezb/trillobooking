984fdcc06d6753690bafddbbfd19346a9e61be6d
Ruben Mavarez <rmavarez@glamit.com.ar>
2021-02-12 10:28:34 -0300

    First commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,11 @@
+
+
+function Main() {
+    return (
+        <main className="hotel-view">
+            Main
+        </main>
+    )
+}
+
+export default Main
---
+++ src/components/Nav.tsx
@@ -0,0 +1,11 @@
+
+
+function Nav() {
+    return (
+        <nav className="sidebar">
+            Navigation
+        </nav>
+    )
+}
+
+export default Nav
