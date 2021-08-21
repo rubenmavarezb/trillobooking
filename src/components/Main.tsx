e2bb387f8b37ba11ed955128bc23eff8495c45a8
Ruben Mavarez <rubenmavarezb@gmail.com>
2021-08-21 12:02:27 -0300

    ultimos cambios

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,75 @@
+import {useState} from 'react';
+import { FaStar, FaLocationArrow } from 'react-icons/fa';
 
+type Image = {
+    id: number;
+    src: string;
+    alt: string;
+}
 
 function Main() {
+
+    const [images] = useState<Image[]>([
+        {
+            id:1,
+            src: 'hotel-1.jpg',
+            alt: 'Photo hotel 1'
+        },
+        {
+            id:2,
+            src: 'hotel-2.jpg',
+            alt: 'Photo hotel 2'
+        },
+        {
+            id:3,
+            src: 'hotel-3.jpg',
+            alt: 'Photo hotel 3'
+        }
+    ]);
+
+    const getRating = (stars:number) => {
+
+        let jsx = [];
+
+        for (let index = 0; index < stars; index++) {
+            jsx.push(<FaStar className="overview__icon-star"/>)
+        }
+
+        return jsx;
+    }
+
     return (
         <main className="hotel-view">
-            Main
+            <div className="gallery">
+                {images.length && images.map((image:Image) => (
+                    <figure className="gallery__item" key={image?.id}>
+                        <img src={require(`../img/${image.src}`).default} alt={image.alt} className="gallery__photo" />
+                    </figure>
+                ))}
+            </div>
+
+            <div className="overview">
+                <h1 className="overview__heading">
+                    Hotel Las Palmas
+                </h1>
+                <div className="overview__stars">
+                    {getRating(5).map((star:any, index:number) => (
+                        <span key={index}>
+                            {star}
+                        </span>
+                    ))}
+                </div>
+
+                <div className="overview__location">
+                    <FaLocationArrow className="overview__icon-location"/>
+                    <button className="btn-inline">Abufeira, Protugal</button>
+                </div>
+
+                <div className="overview__rating">
+                    <div className="overview__rating-average">8.6</div>
+                    <div className="overview__rating-count">429 votes</div>
+                </div>
+            </div>
         </main>
     )
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,61 @@
+import { Fragment, useState } from 'react';
+import { FaHome, FaPlane, FaCar, FaMap } from 'react-icons/fa';
+import { IconType } from 'react-icons'
 
+type LiItem = {
+    id:number;
+    icon:IconType;
+    active: boolean;
+    label:string;
+}
 
 function Nav() {
+
+    const [liItems] = useState<LiItem[]>([
+        {
+            id:1,
+            icon: FaHome,
+            active: true,
+            label: 'Hotel'
+        },
+        {
+            id:2,
+            icon: FaPlane,
+            active: false,
+            label: 'Flight'
+        },
+        {
+            id:3,
+            icon: FaCar,
+            active: false,
+            label: 'Car Rental'
+        },
+        {
+            id:4,
+            icon: FaMap,
+            active: false,
+            label: 'Tours'
+        },
+    ])
+
     return (
         <nav className="sidebar">
-            Navigation
+            <ul className="side-nav">
+                {liItems.length !== 0 ? (
+                    liItems.map((li:LiItem) => (
+                        <li key={li.id} className={li.active ? "side-nav__item side-nav__item--active" : "side-nav__item"}>
+                            <a href="!#" className="side-nav__link">
+                                <li.icon className="side-nav__icon"/>
+                                <span>{li.label}</span>
+                            </a>
+                        </li>
+                    ))
+                ) : <Fragment/>}
+            </ul>
+
+            <div className="legal">
+                &copy; 2021 by Trillo. All rights reseved.
+            </div>
         </nav>
     )
 }
